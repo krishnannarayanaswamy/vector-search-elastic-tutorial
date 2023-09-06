871e6714189038cff6a4b345c66528b0c76f4a34
Krishnan Narayana Swamy <97494161+krishnannarayanaswamy@users.noreply.github.com>
2023-09-06 16:30:33 +1000

    Update import_model.py

--- nlp_models/import_model.py
+++ nlp_models/import_model.py
@@ -7,7 +7,7 @@ from eland.ml.pytorch.transformers import TransformerModel
 ELASTIC_ADDRESS = "https://3c816b713afd4cf08196bc3520542d99.us-central1.gcp.cloud.es.io:443"
 # Uncomment the following lines if start ES with SECURITY ENABLED.
 #ELASTIC_ADDRESS = "https://localhost:9200"
-ELASTIC_PASSWORD = "CzCWVaNQfJbAYEJyyFaoX9Oe"
+ELASTIC_PASSWORD = ""
 CA_CERTS_PATH = "/Users/krishnan/ai-demos/vector-search-elastic-tutorial/A2525B64D8BFD084D946539261844AC9A3F7DBDC.crt"
 
 def main():
